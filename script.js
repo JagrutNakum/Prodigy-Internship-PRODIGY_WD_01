6fe7d54c0b229bb9f3e555fe12cae292b0a21e6d
JagrutNakum <jagrutnakum@gmail.com>
2024-06-21 18:33:04 +0530

    Final Commit

--- script.js
+++ script.js
@@ -1,4 +1,24 @@
 import scrollHandler from "./scripts/header.js";
 
 window.addEventListener("scroll", scrollHandler);
+// window.addEventListener("resize", handerResize);
 
+// function handerResize(){
+
+//     if (window.innerWidth < 1047) {
+//         docu
+//     }
+// }
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var element = document.getElementById('three-bars');
+    element.onclick = function() {
+        myFunction(this);
+    };
+});
+
+function myFunction(x) {
+    x.classList.toggle("change");
+    document.getElementById("verticle-menu").classList.toggle("hide");
+  }
--- scripts/header.js
+++ scripts/header.js
@@ -1,11 +1,10 @@
 
    export default function scrollHandler() {
     if (window.scrollY > 164) {
-        console.log('function executed!')
-        document.getElementById('header').classList.add('header-on-scroll');
+        document.getElementById('nav').classList.add('header-on-scroll');
     } 
     else{
-        document.getElementById('header').classList.remove('header-on-scroll');
+        document.getElementById('nav').classList.remove('header-on-scroll');
     }
 
 }
